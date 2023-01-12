4e93fba6b069ecf154cb38d48fe49574336dda5a
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2023-01-12 23:50:06 +0300

    added animations

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//!    Preloader
+
+window.onload = function () {
+   document.body.classList.add("loaded_hiding");
+   window.setTimeout(function () {
+      document.body.classList.add("loaded");
+      document.body.classList.remove("loaded_hiding");
+   }, 500);
+};
+
+//! Плавная прогрузка Элементов
+
+function onEntry(entry) {
+   entry.forEach((change) => {
+      if (change.isIntersecting) {
+         change.target.classList.add("element-show");
+      }
+   });
+}
+
+let options = {
+   threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+
+for (let elm of elements) {
+   observer.observe(elm);
+}
